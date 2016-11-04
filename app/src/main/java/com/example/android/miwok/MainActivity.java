33d3030a5d060bba759e859c5463737f2b6aa82a
Akash Chavan <meakashchavan@gmail.com>
2016-11-04 15:58:21 +0530

    NumbersActivity update ->
    Add words list to NumbersActivity.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,21 +29,49 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+
+        //find the view that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        TextView family = (TextView)findViewById(R.id.family);
+        TextView colors = (TextView)findViewById(R.id.colors);
+        TextView phrases = (TextView)findViewById(R.id.phrases);
+
+
+
+        //set  a clicklistener on that view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,NumbersActivity.class);
+                startActivity(i);
+            }
+        });
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,FamilyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ColorsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
-    public void openNumbersList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
-    public void openFamilyMembers(View view){
-        Intent i = new Intent(this,FamilyActivity.class);
-        startActivity(i);
-    }
-    public void openColors(View view){
-        Intent i = new Intent(this,ColorsActivity.class);
-        startActivity(i);
-    }
-    public void openPhrases(View view){
-        Intent i = new Intent(this,PhrasesActivity.class);
-        startActivity(i);
-    }
+
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+
+        // array of words
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add( "one");
+        words.add( "two");
+        words.add( "three");
+        words.add( "four");
+        words.add( "five");
+        words.add( "six");
+        words.add( "seven");
+        words.add( "eight");
+        words.add( "nine");
+        words.add( "ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+
+        for(int index = 0; index < words.size(); index++){
+            TextView wordView = new TextView(this);
+
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+        }
     }
 }
